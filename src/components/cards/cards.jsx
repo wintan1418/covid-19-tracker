68b08edd98ae35052aa95f9468d3af98a59128ef
oluwadare <wintan1418@gmail.com>
2021-11-19 12:35:57 +0100

    Add stylings to card component

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import { StylesContext } from '@material-ui/styles';
+import styles from './cards.module.css'
+
 const Cards = (props) => {
  
  console.log(props);
 
   return (
-    <h1>cards</h1>
+   <div className={styles.container}>
+      <h1>cards</h1>
+   </div>
+   
   )
 }
 
