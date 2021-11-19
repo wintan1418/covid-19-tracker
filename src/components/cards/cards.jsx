e5d8609fca477bd31b3ae4c386bd920bd8c69e4c
oluwadare <wintan1418@gmail.com>
2021-11-19 12:49:25 +0100

    Build jsx content for Card

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -9,7 +9,17 @@ const Cards = (props) => {
 
   return (
    <div className={styles.container}>
-      <h1>cards</h1>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Cards}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>Infected-people</Typography>
+            <Typography variant="h5">REAL DATE</Typography>
+            <Typography color="textSecondary">Real Data</Typography>
+            <Typography variant="body2">Number of active Covid-19 cases</Typography>
+          </CardContent>
+         
+         </Grid> 
+      </Grid>
    </div>
    
   )
