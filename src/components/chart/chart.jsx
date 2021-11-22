c01fa9452ea78ebcef0ba43af5c33848bdfb35aa
oluwadare <wintan1418@gmail.com>
2021-11-22 14:15:26 +0100

    Implement chart functionalities

--- src/components/chart/chart.jsx
+++ src/components/chart/chart.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import {Line , Bar} from 'react-chartjs-2'
 
-const Charts = () => {
+const Chart = () => {
+ const [dailyData,setDailyData] = useState({})
+
+ useEffect(() => {
+  const fetchApi = async () => {
+    setDailyData(await fetchDailyData())
+  }
+  fetchApi();
+
+ });
+
+ const lineChart = (
+  dailyData[0]
+  ? (
+    <Line
+    data={{
+      labels: '',
+      datasets: [{}, {}],
+    }}
+    />) : null
+  
+ );
+
   return (
     <h1>chart</h1>
   )
 }
 
-export default Charts;
+export default Chart;
