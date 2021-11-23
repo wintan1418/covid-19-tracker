dd6d1127c296959ced3456799a968248c8accc3f
oluwadare <wintan1418@gmail.com>
2021-11-23 09:01:14 +0100

    Use hooks to set syncinf functions for country picker

--- src/Api/index.js
+++ src/Api/index.js
@@ -32,3 +32,4 @@ export const fetchDailyData = async () => {
     
   } 
 }
+
--- src/components/countryPicker/countryPicker.jsx
+++ src/components/countryPicker/countryPicker.jsx
@@ -1,10 +1,21 @@
 import React, {useState,useEffect} from 'react';
 import {NativeSelect, FormControl} from '@material-ui/core';
-
+import styles from './countryPicker.module.css';
+import {countries} from '../../Api';
 
 const CountryPicker = () => {
+
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  
+  useEffect(()=> {
+    const fetchCountries = async () => {
+      setFetchedCountries(await countries);
+    }
+
+    fetchedCountries();
+  }, [setFetchedCountries]);
   return (
-    <FormControl>
+    <FormControl className={styles.formControl}>
       <NativeSelect>
         <option value="global">Global</option>
       </NativeSelect>
